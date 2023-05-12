fa1d773eef5db2d78712351c545fa4721ad0491c
Irina Scurtu <scurtuirina@gmail.com>
2023-05-12 13:32:45 +0300

    prepared first ex

--- gRPC/BasicSyntax/BasicSyntaxConsumer/Program.cs
+++ gRPC/BasicSyntax/BasicSyntaxConsumer/Program.cs
@@ -1,21 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Google.Protobuf.WellKnownTypes;
-using MyExample.AddressBook;
 
-Console.WriteLine("Hello, World!");
-var addressbook = new AddressBook();
-
-addressbook.People.Add(new Person()
-{
-    Name = "John Doe",
-    Id = 1,
-    Email = "",
-    LastUpdated = Timestamp.FromDateTime(DateTime.UtcNow),
-    Phones = {
-        new Person.Types.PhoneNumber() {
-        Number = "123456789",
-        Type = Person.Types.PhoneType.Mobile
-        } }
-
-});
-addressbook.People.Add(new Person() { Name = "John Doe", Id = 2, Email = "" });
